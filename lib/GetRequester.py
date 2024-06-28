91fb923e639e9dab4c048c57bf97d1161397197e
student <kmac.kimaru93@gmail.com>
2024-06-28 14:13:20 +0300

    adding get request api lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -2,12 +2,19 @@ import requests
 import json
 
 class GetRequester:
-
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        people = json.loads(self.get_response_body())
+
+        return people
+
+url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+
+people = GetRequester(url).load_json()
+print(people)
